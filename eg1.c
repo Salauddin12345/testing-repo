fd57310896f5c5ea22f07e830caebdd865098307
Salauddin Nagori <salanagori333@gmail.com>
2023-03-18 17:21:56 +0530

    first commit

---
+++ eg1.c
@@ -0,0 +1,17 @@
+sd// program to add two number
+// ool
+#include<stdio.h>
+int add(int a,int b)
+{
+return a+b;
+}
+int subt(int a,int b)
+{
+return a-b;
+}
+int main()
+{
+int result=add(2,4);
+printf("result is : %d\n",result);
+return 0;
+}
