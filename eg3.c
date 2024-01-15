35a594fd2c564742ccb055c006bb2165cf9e19ce
Salauddin <salanagori333@gmail.com>
2024-01-15 23:38:41 +0530

    Salauddin : added a new file (eg3.c) with a new sorting technique

---
+++ eg3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void insertion_sort(int *x,int size)
+{
+
+
+}
+
+int main()
+{
+int x[]={13,2322,2,3,1,4,3,0,0,12};
+int size=sizeof(x)/sizeof(int);
+insertion_sort(x,size);
+for(int i=0;i<size;i++) printf("%d\n",size);
+return 0;
+}
